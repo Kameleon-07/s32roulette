b6808b5e871d55c53ab07b013380f16b2ee53da5
dogeowsky <cptcombinedogepl@gmail.com>
2024-02-05 11:23:26 +0100

    Added game idea

--- main.rs
+++ main.rs
@@ -1,3 +1,12 @@
 fn main() {
     println!("Boom, you System32 is non-existent now. Good Luck!");
+
+    // imo mocny przekaz ^
+    /*  ZAMYSŁ:
+
+        1. Menu - wybór trybu gry: SP (PvE), MP (PvP, 1v1) 
+        2. Pierwsza gra przeciwko botu to samouczek
+        3. Powerupy, takie same jak w grze: papierosy (odnawianie życia), piwo (pozwala na przeładowanie, pominięcie kolejki), piła (odcięcie lufy strzelby, podwójne obrażenia), lupa (sprawdzenie załadowanej amunicji), kajdanki (blokują przeciwnika na jedną turę)
+        4. Możliwość postrzelenia siebie samego lub przeciwnika :DDDD
+     */
 }
